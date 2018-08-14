351e0944f1edcbc9824784dffeb891184a94c385
irenewang <13751862433@163.com>
2018-08-14 18:17:33 +0800

    wxh

---
+++ src/main/java/net/kugou/qa/App.java
@@ -0,0 +1,13 @@
+package net.kugou.qa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
